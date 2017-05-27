81ded7141d684651588732fdb40372b9c33afe40
zhangji <zhangji@smartisan.com>
2017-05-27 19:58:10 +0800

    Fix the bug of when read invaild image.
    Signed-off-by: zhangji <zhangji@smartisan.com>

--- ImageSimilarFinder.py
+++ ImageSimilarFinder.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding=utf-8
 import os
-import PIL.Image
 import shutil
 import dhash
 import datetime
+import PIL
+import sys
 # Allow library to be imported even if neither wand or PIL are installed
 try:
     import wand.image
@@ -12,23 +13,33 @@ except ImportError:
     wand = None
 
 try:
-    import PIL.Image
+    from PIL import Image
 except ImportError:
     PIL = None
 
 
-targetDir= "/Users/zhangji/Downloads"
+targetDir= "/media/zhangji/disk2/Code/U1Pro"
 resultDir = targetDir + "/SimilarResults"
 targetDirPathLen = len(targetDir)
-imageSize = 8
+imageSize = 16
 limitDiff = 5
 
 
 def load_image(filename):
+    image = None
     if wand is not None:
-        return wand.image.Image(filename=filename)
+        try:
+          image=wand.image.Image(filename=filename)
+        except:
+          image = None
+        return image
     elif PIL is not None:
-        return PIL.Image.open(filename)
+        try:
+          image=PIL.Image.open(filename)
+        except:
+          image = None
+          print(filename+" is not a invalid img!")
+        return image
     else:
         sys.stderr.write('You must have wand or Pillow/PIL installed to use the dhash command\n')
         sys.exit(1)
@@ -37,23 +48,31 @@ def load_image(filename):
 def findSimilarImgs(baseImageFile, tarDir, hasCmpedList, step):
     count = 0
     image1 = load_image(baseImageFile)
-    hash1 = dhash.dhash_int(image1, size=imageSize)
+    if image1 is None:
+      return
+    try:
+      hash1 = dhash.dhash_int(image1, size=imageSize)
+    except:
+      return
     for path, d, filelist in os.walk(tarDir):
         if (not path.endswith('.git') and (not path.startswith(resultDir))):
             for filename in filelist:
                 if (filename.endswith('jpg') or filename.endswith('png')):
                     count = count + 1
                     imageName = os.path.join(path, filename)
-
                     if (imageName not in hasCmpedList):
                         image2 = load_image(imageName)
-                        hash2 = dhash.dhash_int(image2, size=imageSize)
-                        num_bits_different = dhash.get_num_bits_different(hash1, hash2)
-                        diff = 100 * num_bits_different/(imageSize * imageSize * 2)
-                        if (diff <= limitDiff):
-                            hasCmpedList.append(imageName)
-                            print (baseImageFile +" is same with "+imageName)
-                            movePicToResultDir(step, baseImageFile, imageName, diff)
+                        if image2 is not None:
+                            try:
+                             hash2 = dhash.dhash_int(image2, size=imageSize)
+                            except:
+                              continue
+                            num_bits_different = dhash.get_num_bits_different(hash1, hash2)
+                            diff = 100 * num_bits_different/(imageSize * imageSize * 2)
+                            if (diff <= limitDiff):
+                                hasCmpedList.append(imageName)
+                                print (baseImageFile +" is same with "+imageName)
+                                movePicToResultDir(step, baseImageFile, imageName, diff)
 
 
 def movePicToResultDir(hasCmpedLength, baseImageFile, similarImageFile, diff):
