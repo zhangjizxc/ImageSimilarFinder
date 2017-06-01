f6501b5eb0ffb4988dc56add0b39024523c1138b
zhangji <zhangjizxc@gmail.com>
2017-06-01 12:11:58 +0800

    Add args for use.
    Signed-off-by: zhangji <zhangjizxc@gmail.com>

--- ImageSimilarFinder.py
+++ ImageSimilarFinder.py
@@ -18,13 +18,6 @@ except ImportError:
     PIL = None
 
 
-targetDir= "/media/zhangji/disk2/Code/U1Pro"
-resultDir = targetDir + "/SimilarResults"
-targetDirPathLen = len(targetDir)
-imageSize = 16
-limitDiff = 5
-
-
 def load_image(filename):
     image = None
     if wand is not None:
@@ -49,9 +42,9 @@ def findSimilarImgs(baseImageFile, tarDir, hasCmpedList, step):
     count = 0
     image1 = load_image(baseImageFile)
     if image1 is None:
-      return
+        return
     try:
-      hash1 = dhash.dhash_int(image1, size=imageSize)
+        hash1 = dhash.dhash_int(image1, size=imageSize)
     except:
       return
     for path, d, filelist in os.walk(tarDir):
@@ -87,26 +80,87 @@ def movePicToResultDir(hasCmpedLength, baseImageFile, similarImageFile, diff):
     filenameWithPath2 = moveToDir + "/" + str(diff) +"%_" + filename2
     shutil.copy(similarImageFile, filenameWithPath2)
 
+def isPathInList(list, path):
+    if (len(list) == 1 and list[0] == 'A'):
+        return True
+    for item in list:
+        if (path is not None and path.startswith(item)):
+            return True
+    return False
+
+def arrangePics(targetDir, picDir):
+    for path, d, filelist in os.walk(targetDir):
+        if (not path.endswith('.git')) and isPathInList(targetLimitedSubDirs, path):
+            for filename in filelist:
+                if (filename.endswith('jpg') or filename.endswith('png')):
+                    fileNameWithPath = os.path.join(path, filename)
+                    image = load_image(fileNameWithPath)
+                    if (image is not None):
+                        ratio = format(float(image.height) / float(image.width), '.2f')
+                        tempPath = os.path.join(picDir, str(ratio))
+                        if (not os.path.exists(tempPath)):
+                            os.mkdir(tempPath)
+                        tempFileNameWithPath = os.path.join(tempPath, filename)
+                        shutil.copy(fileNameWithPath, tempFileNameWithPath)
 
 
-if (os.path.exists(resultDir)):
-    print('We need cLear the result dir')
-    shutil.rmtree(resultDir, ignore_errors=False, onerror=None)
+if __name__ == '__main__':
+    import argparse
 
-os.mkdir(resultDir)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--size', type=int, default=16,
+                        help='width and height of dhash image size, default %(default)d')
+    parser.add_argument('-t', '--threshold', type=int, default=5,
+                        help='The threshold to judge whether similar image or not')
+    parser.add_argument('-p', '--path', nargs='?',
+                        help='The path to find.')
+    parser.add_argument('-f', '--filter', default='A', nargs='*',
+                        help='The sub dirs to compare together.')
+    args = parser.parse_args()
 
-hasCmpedList=[]
-step=0
-begin = datetime.datetime.now()
-for path,d,filelist in os.walk(targetDir):
-    if (not path.endswith('.git') and (not path.startswith(resultDir))):
-        for filename in filelist:
-            if (filename.endswith('jpg') or filename.endswith('png')):
-                if (filename not in hasCmpedList):
-                    step = step + 1
-                    imageFileName = os.path.join(path, filename)
-                    hasCmpedList.append(imageFileName)
-                    findSimilarImgs(imageFileName, targetDir, hasCmpedList, step)
+    if (args.path is None):
+        sys.stderr.write('You must specified path.\n')
+        sys.exit(1)
 
-end = datetime.datetime.now()
-print("Total Used Time: "+str(end-begin))
+    targetDir = args.path
+    targetLimitedSubDirs=[]
+    for subdir in args.filter:
+        targetLimitedSubDirs.append(subdir)
+    # targetLimitedSubDirs= [targetDir+'/frameworks', targetDir+'/packages']
+    print targetLimitedSubDirs
+    resultDir = targetDir + "/../SimilarImgResults"
+    tempDir = resultDir + "/AllPics"
+    targetDirPathLen = len(targetDir)
+    imageSize = 16
+    threshold = 5
+
+    if (os.path.exists(resultDir)):
+        print('We need cLear the result dir')
+        shutil.rmtree(resultDir, ignore_errors=False, onerror=None)
+    os.mkdir(resultDir)
+    os.mkdir(tempDir)
+
+    hasCmpedList = []
+    step = 0
+    begin = datetime.datetime.now()
+    arrangePics(targetDir, tempDir)
+    # for path,d,filelist in os.walk(targetDir):
+    #     if (not path.endswith('.git')) and isPathInList(targetLimitedSubDirs, path):
+    #         for filename in filelist:
+    #             if (filename.endswith('jpg') or filename.endswith('png')):
+    #                 fileNameWithPath = os.path.join(path, filename)
+    #                 image = load_image(fileNameWithPath)
+    #                 if (image is not None):
+    #                     ratio = format(float(image.height)/float(image.width),'.2f')
+    #                     tempPath = os.path.join(tempDir, str(ratio))
+    #                     if (not os.path.exists(tempPath)):
+    #                         os.mkdir(tempPath)
+    #                     tempFileNameWithPath= os.path.join(tempPath,filename)
+    #                     shutil.copy(fileNameWithPath, tempFileNameWithPath)
+    #                 # if (filename not in hasCmpedList):
+    #                 #     step = step + 1
+    #                 #     hasCmpedList.append(imageFileName)
+    #                 #     findSimilarImgs(imageFileName, targetDir, hasCmpedList, step)
+
+    end = datetime.datetime.now()
+    print("Total Used Time: " + str(end - begin))
